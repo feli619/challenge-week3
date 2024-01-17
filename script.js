120da77201429377f54fc75b1fff6e4dac689150
feli619 <152445873+feli619@users.noreply.github.com>
2024-01-16 23:49:39 -0500

    added JS code for functioning button

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 // Assignment code here
 
+function generate(){
+  // get references of all the inputs
+  const length = document.getElementById("length").value;
+  const numbers = document.getElementById("numbers").checked;
+  const uppercase = document.getElementById("uppercase").checked;
+  const lowercase = document.getElementById("lowercase").checked;
+  const symbols = document.getElementById("symbols").checked;
+  
+  // generate random password
+  const password = generatePassword(parseInt(length), numbers, uppercase, lowercase, symbols)
+  // show the password
+  document.getElementById("password").innerHTML = password
+  alert("Your Secure Password: " + password)
+  console.log(password)
+};
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword(length, numbers, uppercase, lowercase, symbols) {
+  let characters = " ";
+  
+  if (numbers) {
+    characters += "0123456789"
+  }
+  if (uppercase) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (lowercase) {
+    characters += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (symbols) {
+    characters += "!#$%&()*+,-./:;<=>?@[^_`{|}~"
+  }
+  
+  let passwordArray = [];
+  while (passwordArray.length < length){
+    const characters = characters[Math.floor(Math.random() * characters.length)];
+    passwordArray.push(character);
+  }
+  return passwordArray.join('');
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// document.getElementById("generate").addEventListener("click", function (
